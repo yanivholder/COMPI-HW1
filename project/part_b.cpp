d454436a988bc293f195e2cfda80ad1343c7e21c
Yaniv Holder <yaniv.holder@outlook.com>
2021-04-13 10:15:41 +0100

    part_b finished before testing

--- project/part_b.cpp
+++ project/part_b.cpp
@@ -1,10 +1,64 @@
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <iostream>
+
 #include "tokens.hpp"
+using namespace std;
+extern int yylex();
+
+void print_tabs(int num)
+{
+    for(int i = 0; i < num; i++)
+    {
+        cout << "\t";
+    }
+}
 
 int main()
 {
 	int token;
+	std::vector<std::string> p_vec;
+
 	while(token = yylex()) {
-	// Your code here
+        switch (token) {
+            case LPAREN:
+                print_tabs(p_vec.size());
+                cout << "(" << endl;
+                p_vec.emplace_back("(");
+                break;
+            case LBRACE:
+                print_tabs(p_vec.size());
+                cout << "{" << endl;
+                p_vec.emplace_back("{");
+                break;
+            case RPAREN:
+                if (p_vec[p_vec.size() - 1] != "(") {
+                    cout << "Error: Bad Expression" << endl;
+                    exit(0);
+                } else {
+                    p_vec.pop_back();
+                    print_tabs(p_vec.size());
+                    cout << ")" << endl;
+                }
+                break;
+            case RBRACE:
+                if (p_vec[p_vec.size() - 1] != "{") {
+                    cout << "Error: Bad Expression" << endl;
+                    exit(0);
+                } else {
+                    p_vec.pop_back();
+                    print_tabs(p_vec.size());
+                    cout << "}" << endl;
+                }
+                break;
+            case ERROR:
+                cout << "Error " << yytext[yyleng - 1] << endl;
+                exit(0);
+            default:
+                cout << "Error: " << yytext << endl;
+                exit(0);
+        }
 	}
 	return 0;
 }
